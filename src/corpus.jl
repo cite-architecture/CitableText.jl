bb05eff1677467a178de2b5d4e88255ed8534409
Neel Smith <nsmith@Technites.local>
2021-01-05 07:30:02 -0500

    Fix typo

--- src/corpus.jl
+++ src/corpus.jl
@@ -6,6 +6,6 @@ end
 function to_df(c::CitableCorpus)
     urns = map(cn -> cn.urn, c.corpus)
     psgs = map(cn -> cn.text, c.corpus)
-    df = DataFrame (urn = urns,text = psgs)
+    df = DataFrame(urn = urns,text = psgs)
     df
 end
